a24219c214ccddf15db7b0fadc0f0eeaad9f76f9
Ma-Pol <ysh.skyblue6485@gmail.com>
2023-05-13 13:15:36 +0900

    [feat]  결제 전처리, 결제, 결제 후처리 기능 구현

--- src/main/java/com/hobbyvillage/backend/user_purchase/UserPruchaseService.java
+++ src/main/java/com/hobbyvillage/backend/user_purchase/UserPruchaseService.java
@@ -1,10 +1,26 @@
 package com.hobbyvillage.backend.user_purchase;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface UserPruchaseService {
+	String getImportToken() throws IOException;
 	int getProductState(String prodCode);
+	int checkProductInfo(String prodCode, int prodPrice, int prodShipping, String prodHost, int period);
 	UserPurchaseUserDTO getUserInfo(String email);
 	List<UserPurchaseAddressDTO> getAddressList(String email);
 	List<UserPurchaseCouponDTO> getCouponList(String email);
+	int updateProductState(String prodCode);
+	String getPrevOdrNumber(String prodCode);
+	int purchasePreProcess(UserPurchaseProcessDTO data, String prevPage);
+	int paymentsPrepare(String odrNumber, int amount, String token) throws IOException;
+	int compareToDatabase(String odrNumber, int paid_amount, String prevPage);
+	int compareToImport(String odrNumber, int paid_amount, String prevPage, String imp_uid, String token) throws IOException;
+	int increaseRentalCount(String prodCode, int rentalCount);
+	int decreaseSavedMoney(String email, int exactSavedMoney);
+	int deleteCoupon(String email, int couponCode);
+	int purchaseFinalProcess(UserPurchaseProcessDTO data, String prevPage);
+	int updateProductStateFailed(String prodCode);
+	int deleteOrder(String odrNumber, String prevPage);
+	int cancelOrder(String imp_uid, String token) throws IOException;
 }
